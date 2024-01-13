8372b6cf6652578e9921ef295d7ee7b696b7595f
Wendell Centeno <dellortiz06@gmail.com>
2024-01-13 20:37:55 +0800

    Initial commit

---
+++ src/main/java/com/mycompany/infotechCMS/ApplicationController.java
@@ -0,0 +1,16 @@
+package com.mycompany.infotechCMS;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ApplicationController {
+
+    @GetMapping("/index")
+    public String home(){ return "index";}
+
+    @GetMapping("/widgets")
+    public String widget() {return "widgets";}
+
+}
---
+++ src/main/java/com/mycompany/infotechCMS/InfotechCmsApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.infotechCMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfotechCmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfotechCmsApplication.class, args);
+	}
+
+}
